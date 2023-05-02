3235aeae524d6faf813143ccf9f03c18e969fa7f
antogit-sys <antonioperrgiorgi@gmail.com>
2023-05-02 22:32:13 +0200

    use class ftpMan

--- ftpd.py
+++ ftpd.py
@@ -11,25 +11,38 @@
 
 import os
 import sys
+import ftp_management as ftpm
+from time import sleep
 
 def main(argc, argv):
 	NAME = sys.argv[0]
+	#PLIST = '/System/Library/LaunchDaemons/ftp.plist'
+	#PLIST = "prova"
 	if argc == 1:
 		helper(NAME)
 	elif argc == 2:
 		if not isRoot():
 			print(NAME+": only root can run this script\n")
 		else:
-			OP = argv[1]
-			if OP == "status":
-				pass #ftp_status()
-			elif OP == "on":
-				pass #ftp_on()
-			elif OP == "off":
-				pass #ftp_off()
+			fm = ftpm.ftpMan()
+			if fm.isFile:
+				OP = argv[1]
+				if OP == "status":
+					print(fm.getStatus())
+					pass #ftp_status()
+				elif OP == "on":
+					pass #ftp_on()
+				elif OP == "off":
+					pass #ftp_off()
+				else:
+					print(NAME+": operation not allowed, use on/off/status")
 			else:
-				print(NAME+": operation not allowed, use on/off/status")
-
+				print(NAME+": ftp.plist daemon not found")
+				sleep(1)
+				print("ftp.plist Download && Install ...")
+				sleep(2)
+				fm.download_plist()
+				print(NAME+": run the following script again!!!")
 def helper(NAME):
 	print(NAME+":")
 	print("\tsimple script to on/off or see the status of ftp server")
@@ -48,6 +61,9 @@ def isRoot():
 	if os.geteuid() == 0:
 		done = -1
 	return done
-	
+
+def isFile(path):
+	return os.path.isfile(path)
+
 if __name__ == "__main__":
 	sys.exit(main(len(sys.argv), sys.argv))
