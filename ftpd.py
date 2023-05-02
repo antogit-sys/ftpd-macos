da41ae4aa7dc5d22583f2f70ad7adc09c5d4976e
antogit-sys <antonioperrgiorgi@gmail.com>
2023-05-02 19:58:12 +0200

    menu option

--- ftpd.py
+++ ftpd.py
@@ -13,7 +13,32 @@ import os
 import sys
 
 def main(argc, argv):
-	pass
+	NAME = argv[0]
+	if argc == 1:
+		helper(NAME)
+	elif argc == 2:
+		OP = argv[1]
+		if OP == "status":
+			pass #ftp_status()
+		elif OP == "on":
+			pass #ftp_on()
+		elif OP == "off":
+			pass #ftp_off()
+		else:
+			print(NAME+": operation not allowed, use on/off/status")
+
+def helper(NAME):
+	print(NAME+":")
+	print("\tsimple script to on/off or see the status of ftp server")
+	print("usage:")
+	flag = ""	
+	if "./" not in NAME:
+		flag = "python3 "
+	NAME = flag+NAME
+	print("\t"+NAME+" [status|on|off]")
+	print("example:")
+	print("\t"+NAME+" on")
+	print("\t"+NAME+" status")
 
 
 if __name__ == "__main__":
